afa399f425a6f74efb3c30b851d64ed65598d24d
Tom Metcalfe <t.metcalfe@rasa.com>
2018-12-19 14:56:28 +0100

    Add travis config, NLU test script

---
+++ test_nlu.py
@@ -0,0 +1,11 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+
+def test_train_and_persist():
+    training_data = load_data("data/nlu_data.md")
+    trainer = Trainer(config.load("nlu_config.yml"))
+    interpreter = trainer.train(training_data)
+    test_model_directory = trainer.persist("./models/nlu", fixed_model_name="test")
+    assert interpreter.parse('hello') is not None
+    assert test_model_directory
